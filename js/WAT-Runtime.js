d394e8bfcf63990c969016ed62905484505a336b
Andreas Rozek <A.Rozek@gmx.de>
2022-06-19 07:41:08 +0200

    Update WAT-Runtime.js

--- js/WAT-Runtime.js
+++ js/WAT-Runtime.js
@@ -8774,6 +8774,7 @@ var WAT;
                         _a.sent();
                         WAT_isRunning = true;
                         invokeAllRunningFunctionsToCall();
+debugger
                         if (Designer != null) {
                             $('.WAT.Applet').each(function () {
                                 Designer.createDesignerButtonForApplet(VisualOfElement(this));
