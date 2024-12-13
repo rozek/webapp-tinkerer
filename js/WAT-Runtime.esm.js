c7d6bff241e8be63eb743e74038c2ea40b8a56ff
Andreas Rozek <A.Rozek@gmx.de>
2024-12-13 21:09:27 +0100

    Update WAT-Runtime.esm.js

--- js/WAT-Runtime.esm.js
+++ js/WAT-Runtime.esm.js
@@ -8586,7 +8586,7 @@ builtInWidgetTypes['TextTab'] = WAT_TextTab;
 appendStyle(`
   .WAT.Widget > .WAT.TextTab {
     display:block; position:absolute;
-    left:0px; top:0px: right:auto; bottom:0px; width:auto; height:auto;
+    left:0px; top:0px; right:auto; bottom:0px; width:auto; height:auto;
     border:none; border-bottom:solid 2px transparent;
     font-weight:bold;
   }
