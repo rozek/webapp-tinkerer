f72f11e6c782f9020107aad495dd6ff7bf8b3dca
Andreas Rozek <A.Rozek@gmx.de>
2024-12-14 06:00:25 +0100

    Update WAT-Runtime.ts

--- src/WAT-Runtime.ts
+++ src/WAT-Runtime.ts
@@ -9444,7 +9444,7 @@ console.warn('"onDrop" Callback Failure',Signal)
   appendStyle(`
   .WAT.Widget > .WAT.TextTab {
     display:block; position:absolute;
-    left:0px; top:0px; right:auto; bottom:0px; width:auto; height:auto;
+    left:0px; top:0px; right:0px; bottom:0px; width:auto; height:auto;
     border:none; border-bottom:solid 2px transparent;
     font-weight:bold;
   }
@@ -9511,7 +9511,7 @@ console.warn('"onDrop" Callback Failure',Signal)
 
   appendStyle(`
   .WAT.Widget > .WAT.IconTab {
-    left:0px; top:0px; right:auto; bottom:0px; width:auto; height:auto;
+    left:0px; top:0px; right:0px; bottom:0px; width:auto; height:auto;
     border:none; border-bottom:solid 2px transparent;
 
     -webkit-mask-size:contain;           mask-size:contain;
